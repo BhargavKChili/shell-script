d2bf7eb74a734fe611224b79a3c33a6c54acdc1f
BhargavKChili <bk9205@gmail.com>
2024-04-26 17:04:14 +0200

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#declaring of variable
+Person1=Balan  #no space b/w =and value
+Person2=Vandana
+
+#referring the variable
+echo "$Person1:: Hello $Person2, How are you?"
+echo "$Person2:: Hi $Person1 I am good. How are you?"

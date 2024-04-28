93ae3d935ef97c29e640bebd0605bbb8f9be23ef
BhargavKChili <bk9205@gmail.com>
2024-04-28 09:03:00 +0200

    array changes

--- 06.array.sh
+++ 06.array.sh
@@ -5,8 +5,4 @@ MOVIES=("RRR" "KGF" "SALAR")
 #index are 0,1,2..
 # list always start with 0
 
-echo "First Movie is:${MOVIES[0]}"
-echo "First Movie is:${MOVIES[2]}"
 echo "First Movie is:${MOVIES[@]}"
-echo "First Movie is:${MOVIES[1]}"
-

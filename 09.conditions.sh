3ed449eddea6ee92b520242efb8c09d29cacc5b7
BhargavKChili <bk9205@gmail.com>
2024-04-29 18:04:05 +0200

    conditions

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -8,3 +8,11 @@ NUMBER=$1
     else
     echo "Given number $NUMBER is less than 10"
     fi
+
+NUMBER=$2
+if [ $NUMBER -lt 10]
+then
+echo "Given number $NUMBER is less than 10"
+else
+echo "Given number $NUMBER is greater than 10"
+fi
